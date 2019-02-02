5f03da8751ad1931c59b2ad45bb22dcf38b6e26b
Jeffrey <jeffrey.a.egan@gmail.com>
2019-02-02 13:04:49 -0500

    Light GBM Model improvements and submission builder, more extracted features, TODO train with all training data and continue refinement or parameters

--- vsb_make_submission.py
+++ vsb_make_submission.py
@@ -1,52 +1,124 @@
 
 import os
 import time
-from datetime import datetime
-import pandas as pd
 import numpy as np
+import pandas as pd
+import lightgbm as lgb
+from datetime import datetime
+from matplotlib import pyplot as plt
 
-def load_feature_data(file_name):
-    df = pd.read_csv(file_name)
-    return df
+
+def split_data(features, labels, random_state_value=1):
+    from sklearn.model_selection import train_test_split
+    # Using standard split of 80-20 training to testing data split ratio and fixing random_state=1 for repeatability
+    x_train, x_test, y_train, y_test = train_test_split(features, labels, train_size=0.8, test_size=0.2, random_state=random_state_value)
+    return x_train, x_test, y_train, y_test
 
 
-def classification_random_forest_model(features, labels, n_value):
-    from sklearn.ensemble import RandomForestClassifier
-    classifier = RandomForestClassifier(n_estimators=n_value)  # Create Gaussian Classifier
-    classifier.fit(features, labels.values.ravel())
-    #y_predicted = classifier.predict(x_test)
+def classification_light_gbm_model(df_train):
+    print('Loading data...')
+
+    features = ["entropy", "n5", "n25", "n75", "n95", "median", "mean", "std", "var", "rms", "no_zero_crossings", "no_mean_crossings", "min_height", "max_height", "mean_height", "min_width", "max_width", "mean_width", "num_detect_peak", "num_true_peaks"]
+    target = ["fault"]
+    x_train, x_test, y_train, y_test = split_data(df_train[features], df_train[target], 2019)  # Split Data
+
+    print("preparing validation datasets")
+    xgtrain = lgb.Dataset(x_train, y_train)
+    xgtest = lgb.Dataset(x_test, y_test)
+
+    evals_results = {}
+
+    dtrain = lgb.Dataset(x_train, label=y_train)  # Data set used to train model
+    training_iterations = 10 # Number of iterations used to train model
+    metrics = 'auc'
+
+    lgb_params = {
+        'objective': 'binary',
+        'metric': metrics,
+        'learning_rate': 0.01,
+        #'is_unbalance': 'true',  #because training data is unbalance (replaced with scale_pos_weight)
+        'num_leaves': 31,  # we should let it be smaller than 2^(max_depth)
+        'max_depth': -1,  # -1 means no limit
+        'min_child_samples': 20,  # Minimum number of data need in a child(min_data_in_leaf)
+        'max_bin': 255,  # Number of bucketed bin for feature values
+        'subsample': 0.6,  # Subsample ratio of the training instance.
+        'subsample_freq': 0,  # frequence of subsample, <=0 means no enable
+        'colsample_bytree': 0.3,  # Subsample ratio of columns when constructing each tree.
+        'min_child_weight': 5,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
+        'subsample_for_bin': 200000,  # Number of samples for constructing bin
+        'min_split_gain': 0,  # lambda_l1, lambda_l2 and min_gain_to_split to regularization
+        'reg_alpha': 0,  # L1 regularization term on weights
+        'reg_lambda': 0,  # L2 regularization term on weights
+        'nthread': 4,
+        'verbose': 0,
+        'scale_pos_weight':99, # because training data is extremely unbalanced 
+        'boosting_type': 'gbdt',
+        'boost_from_average': False
+    }
+
+    print("Training...")
+    classifier = lgb.train(lgb_params, 
+                     xgtrain, 
+                     valid_sets=[xgtrain, xgtest], 
+                     valid_names=['train','test'], 
+                     evals_result=evals_results, 
+                     num_boost_round=500,
+                     early_stopping_rounds=30,
+                     verbose_eval=True, 
+                     feval=None)
+
+    
+    n_estimators = classifier.best_iteration
+    y_pred_probs = classifier.predict(x_test, n_estimators)
+
+    blues = ["#66D7EB", "#51ACC5", "#3E849E", "#2C5F78", "#1C3D52", "#0E1E2B"]
+    fig=plt.figure(figsize=(14, 8), dpi= 120, facecolor='w', edgecolor='k')
+    plt.hist(y_pred_probs, bins=100, color=blues[1])
+    plt.ylabel("Occurrences")
+    plt.xlabel("Probability of Assigning Fault")
+    plt.savefig("plots/fault_probs_lgbm_submit.png", bbox_inches='tight')
+
+    print("\nModel Report")
+    print("n_estimators : ", n_estimators)
+    print(metrics+":", evals_results['test'][metrics][n_estimators-1])
     return classifier
 
-def classification_random_forest_predict(model, x_test):
-    y_predicted = model.predict(x_test)
+
+def predict_light_gbm_model(classifier, df_test, threshold):
+    print('Predicting...')
+    test_features = ["entropy", "n5", "n25", "n75", "n95", "median", "mean", "std", "var", "rms", "no_zero_crossings", "no_mean_crossings", "min_height", "max_height", "mean_height", "min_width", "max_width", "mean_width", "num_detect_peak", "num_true_peaks"]
+    n_estimators = classifier.best_iteration
+    y_pred_probs = classifier.predict(df_test[test_features], n_estimators)
+
+    y_predicted = []
+    for y in y_pred_probs:  # use probabilities to assign binary classification
+        if y >= threshold:
+            y_predicted.append(1)
+        else:
+            y_predicted.append(0) 
     return y_predicted
 
 
-# Train Model with Full Set
+# Train Model with Full Set, Return Classifier Model
 dwt = "db4"
-number_estimators = 50
+peak_thresh = "0.63"
 
-training_data = "/home/jeffrey/repos/VSB_Power_Line_Fault_Detection/extracted_features/train_features_thresh_0.71_"+dwt+".csv"
+training_data = "/home/jeffrey/repos/VSB_Power_Line_Fault_Detection/extracted_features/train_features_thresh_"+peak_thresh+"_"+dwt+".csv"
 df_train = pd.read_csv(training_data)
-features = df_train[["entropy", "n5", "n25", "n75", "n95", "median", "mean", "std", "var", "rms", "no_zero_crossings", "no_mean_crossings", "min_height", "max_height", "mean_height", "min_width", "max_width", "mean_width", "num_detect_peak", "num_true_peaks"]]
-labels = df_train[["fault"]]
-classifier = classification_random_forest_model(features, labels, number_estimators)
+classifier = classification_light_gbm_model(df_train)  # Light GBM
 
 
 # Make Predictions
-test_data = "/home/jeffrey/repos/VSB_Power_Line_Fault_Detection/extracted_features/test_features_thresh_0.71_"+dwt+".csv"
-test_meta = "/home/jeffrey/repos/VSB_Power_Line_Fault_Detection/source_data/metadata_test.csv"
+test_data = "/home/jeffrey/repos/VSB_Power_Line_Fault_Detection/extracted_features/test_features_thresh_"+peak_thresh+"_"+dwt+".csv"
 df_test = pd.read_csv(test_data).drop(['Unnamed: 0'],axis=1)
-test_features = df_test[["entropy", "n5", "n25", "n75", "n95", "median", "mean", "std", "var", "rms", "no_zero_crossings", "no_mean_crossings", "min_height", "max_height", "mean_height", "min_width", "max_width", "mean_width", "num_detect_peak", "num_true_peaks"]]
-predicted_faults = classification_random_forest_predict(classifier, test_features)
+
+fault_detection_threshold = 0.98
+predicted_faults = predict_light_gbm_model(classifier, df_test, fault_detection_threshold)
 df_test["fault"] = predicted_faults
 
 
 # Make Submission File
-submission_filename = "prediction_submission_"+dwt+".csv"
-
-#submission = df_test[["signal_id", "fault"]]
-#submission.to_csv(submission_filename, sep=",", index="False")
+submission_filename = "prediction_submission_"+peak_thresh+"_"+dwt+"_"+str(fault_detection_threshold)+"fdt_.csv"
 
 f_o = open(submission_filename, "w+")
 f_o.write("signal_id,target\n")
--- vsb_monte_LightGBM.py
+++ vsb_monte_LightGBM.py
@@ -1,9 +1,13 @@
 import numpy as np
 import pandas as pd
 import lightgbm as lgb
+import time  #
+import gc  #
 from sklearn.metrics import matthews_corrcoef
 
 from sklearn.model_selection import GridSearchCV
+
+from matplotlib import pyplot as plt
 
 
 def load_feature_data(file_name):
@@ -28,52 +32,99 @@ def run_light_gbm(data_file):
     print('Loading data...')
     df = load_feature_data(data_file)
 
-    features = df[["entropy", "n5", "n25", "n75", "n95", "median", "mean", "std", "var", "rms", "no_zero_crossings", "no_mean_crossings", "min_height", "max_height", "mean_height", "min_width", "max_width", "mean_width", "num_detect_peak", "num_true_peaks"]]
-    labels = df[["fault"]]
+    features = ["entropy", "n5", "n25", "n75", "n95", "median", "mean", "std", "var", "rms", "no_zero_crossings", "no_mean_crossings", "min_height", "max_height", "mean_height", "min_width", "max_width", "mean_width", "num_detect_peak", "num_true_peaks"]
+    target = ["fault"]
 
-    x_train, x_test, y_train, y_test = split_data(features, labels, 2019)  # Split Data
+    x_train, x_test, y_train, y_test = split_data(df[features], df[target], 2019)  # Split Data
 
-    data_training = lgb.Dataset(x_train, label=y_train)  # Data set used to train model
+    print("preparing validation datasets")
+    xgtrain = lgb.Dataset(x_train, y_train)
+    xgtest = lgb.Dataset(x_test, y_test)
+
+    evals_results = {}
+
+
+
+    dtrain = lgb.Dataset(x_train, label=y_train)  # Data set used to train model
     training_iterations = 10 # Number of iterations used to train model
-    params = {
-        'boosting_type': 'gbdt',
+    metrics = 'auc'
+
+    lgb_params = {
         'objective': 'binary',
-        'metric': 'binary_logloss',
-        #'metric': mcc,
-        #'early_stopping_rounds': 5,
-        'num_leaves': 31,
-        'min_data': 50,
-        'max_depth': 10,
-        'learning_rate': 0.05,
-        'feature_fraction': 0.9,
-        'bagging_fraction': 0.8,
-        'bagging_freq': 5,
-        'verbose': 1
+        'metric': metrics,
+        'learning_rate': 0.01,
+        #'is_unbalance': 'true',  #because training data is unbalance (replaced with scale_pos_weight)
+        'num_leaves': 31,  # we should let it be smaller than 2^(max_depth)
+        'max_depth': -1,  # -1 means no limit
+        'min_child_samples': 20,  # Minimum number of data need in a child(min_data_in_leaf)
+        'max_bin': 255,  # Number of bucketed bin for feature values
+        'subsample': 0.6,  # Subsample ratio of the training instance.
+        'subsample_freq': 0,  # frequence of subsample, <=0 means no enable
+        'colsample_bytree': 0.3,  # Subsample ratio of columns when constructing each tree.
+        'min_child_weight': 5,  # Minimum sum of instance weight(hessian) needed in a child(leaf)
+        'subsample_for_bin': 200000,  # Number of samples for constructing bin
+        'min_split_gain': 0,  # lambda_l1, lambda_l2 and min_gain_to_split to regularization
+        'reg_alpha': 0,  # L1 regularization term on weights
+        'reg_lambda': 0,  # L2 regularization term on weights
+        'nthread': 4,
+        'verbose': 0,
+        'scale_pos_weight':99, # because training data is extremely unbalanced 
+        'boosting_type': 'gbdt',
+        'boost_from_average': False
     }
 
-    print('Starting training...')
-    clf = lgb.train(params, data_training, training_iterations)  # Train model
+    print("Training...")
+    classifier = lgb.train(lgb_params, 
+                     xgtrain, 
+                     valid_sets=[xgtrain, xgtest], 
+                     valid_names=['train','test'], 
+                     evals_result=evals_results, 
+                     num_boost_round=500,
+                     early_stopping_rounds=30,
+                     verbose_eval=True, 
+                     feval=None)
+
     
-    print('Starting predicting...')
-    #y_predicted = clf.predict(x_test, num_iteration=clf.best_iteration_)
-    y_pred_probs = clf.predict(x_test) # output is a list of probabilities
-    y_predicted = []
+    n_estimators = classifier.best_iteration
+    y_pred_probs = classifier.predict(x_test, n_estimators)
 
-    for y in y_pred_probs:  # use probabilities to assign binary classification
-        if y >= 0.5:
-            y_predicted.append(1)
-        else:
-            y_predicted.append(0)
+    blues = ["#66D7EB", "#51ACC5", "#3E849E", "#2C5F78", "#1C3D52", "#0E1E2B"]
+    fig=plt.figure(figsize=(14, 8), dpi= 120, facecolor='w', edgecolor='k')
+    plt.hist(y_pred_probs, bins=100, color=blues[1])
+    plt.ylabel("Occurrences")
+    plt.xlabel("Probability of Assigning Fault")
+    plt.savefig("plots/fault_probs_lgbm.png", bbox_inches='tight')
 
+    print("\nModel Report")
+    print("n_estimators : ", n_estimators)
+    print(metrics+":", evals_results['test'][metrics][n_estimators-1])
 
-
-    # Evaulation / Performance Scoring
-    m_mcc = mcc(y_test, y_predicted)
-    print("Matthews Correlation Coefficient score for predictions: "+str(m_mcc))
-
-    # Important Features
-    #print('Feature importances:', list(clf.feature_importances_))
+    return classifier, y_test, y_pred_probs
 
 
-data_file = "/home/jeffrey/repos/VSB_Power_Line_Fault_Detection/extracted_features/"+"train_features_thresh_0.69_db4.csv"
-run_light_gbm(data_file)
+
+def survey_thresholds(y_test, y_pred_probs):
+
+    thresholds = np.linspace(0.01, 1.00, 100)
+    mcc = []
+    for threshold in thresholds:
+        y_predicted = []
+        for y in y_pred_probs:  # use probabilities to assign binary classification
+            if y >= threshold:
+                y_predicted.append(1)
+            else:
+                y_predicted.append(0)
+        mcc.append(matthews_corrcoef(y_test, y_predicted))
+        print("MCC: "+str(matthews_corrcoef(y_test, y_predicted)))
+
+    blues = ["#66D7EB", "#51ACC5", "#3E849E", "#2C5F78", "#1C3D52", "#0E1E2B"]
+    fig=plt.figure(figsize=(14, 8), dpi= 120, facecolor='w', edgecolor='k')
+    plt.plot(thresholds, mcc, color=blues[1])
+    plt.ylabel("Fault Classification Threshold")
+    plt.xlabel("Matthews Correlation Coefficient Score")
+    plt.savefig("plots/mcc_vs_faultThresh_lgbm.png", bbox_inches='tight')
+
+
+data_file = "/home/jeffrey/repos/VSB_Power_Line_Fault_Detection/extracted_features/"+"train_features_thresh_0.63_db4.csv"
+classifier, y_test, y_pred_probs = run_light_gbm(data_file)
+survey_thresholds(y_test, y_pred_probs)
